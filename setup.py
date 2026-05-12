91fbbf3ba5e90ecd25b122fc6ad256720476c54e
shivam19042001 <18ec.shivamthacker@gmail.com>
2026-05-12 23:11:39 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Shivam Thacker',
+    author_email= 'thackershivam555@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
